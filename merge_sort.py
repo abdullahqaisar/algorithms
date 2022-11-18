ff5a31b65a47f4544d4d4d43413224e1addc527f
abdullahqaisar <abdullahqaisarr@gmail.com>
2022-11-18 23:28:21 +0500

    merge sort example

--- merge_sort.py
+++ merge_sort.py
@@ -1,38 +1,25 @@
+
 def merge_sort(arr):
-    if len(arr) > 1:
+    if (len(arr)>1):
+
         mid = len(arr)//2
 
         L = arr[:mid]
- 
         R = arr[mid:]
- 
+
         merge_sort(L)
-
         merge_sort(R)
- 
-        i = j = k = 0
-
-        while i < len(L) and j < len(R):
-            if L[i] <= R[j]:
-                arr[k] = L[i]
-                i += 1
-            else:
-                arr[k] = R[j]
-                j += 1
-            k += 1
- 
-
-        while i < len(L):
-            arr[k] = L[i]
-            i += 1
-            k += 1
- 
-        while j < len(R):
-            arr[k] = R[j]
-            j += 1
-            k += 1
 
 arr = [1, 20, 51, 2, 9, 10]
-
-
 merge_sort(arr)
+
+
+# arr = [1, 20, 51, 2, 9, 10]
+
+# L=[1, 20, 51]                   R=[2, 9, 10]
+
+# L=[1]  R=[20, 51]               L=[2]      R=[9, 10]
+
+#        L=[20] R=[51]                     L=[9] R=[10]
+
+
