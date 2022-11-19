3be0b8fa9c910caf5234e65b41c387ddae9daf72
abdullahqaisar <abdullahqaisarr@gmail.com>
2022-11-19 23:09:49 +0500

    merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,6 +1,6 @@
 
 def merge_sort(arr):
-    if (len(arr)>1):
+    if (len(arr) > 1):
 
         mid = len(arr)//2
 
@@ -9,17 +9,26 @@ def merge_sort(arr):
 
         merge_sort(L)
         merge_sort(R)
+        i = j = k = 0
+
+        if (i < len(L)) and (j < len(R)):
+            pass
+
 
 arr = [1, 20, 51, 2, 9, 10]
 merge_sort(arr)
 
 
-# arr = [1, 20, 51, 2, 9, 10]
+# arr = [100, 20, 51, 2, 9, 10]
 
-# L=[1, 20, 51]                   R=[2, 9, 10]
+# L=[100, 20, 51]                   R=[2, 9, 10]
 
-# L=[1]  R=[20, 51]               L=[2]      R=[9, 10]
+# L=[100]  R=[20, 51]               L=[2]      R=[9, 10]
 
 #        L=[20] R=[51]                     L=[9] R=[10]
 
+# L=[100]  R=[20, 51]               L=[2]      R=[9, 10]
 
+# L=[100, 20, 51]                   R=[2, 9, 10]
+
+# arr = [100, 20, 51, 2, 9, 10]
